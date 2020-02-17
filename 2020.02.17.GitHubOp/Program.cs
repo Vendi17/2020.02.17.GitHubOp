cdda8fff9fbba344b5a80080e2f7802f9bb8f517
Gáspár Vendel <g.vendi17@gmail.com>
2020-02-17 08:28:04 +0100

    Add project files.

---
+++ 2020.02.17.GitHubOp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020._02._17.GitHubOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
