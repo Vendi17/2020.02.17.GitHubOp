73ffcd58b48cd1c531e450a69c7224e0e1bed80b
Gáspár Vendel <g.vendi17@gmail.com>
2020-02-17 09:11:12 +0100

    Made the second class for a list and for the streamReader. In the first class called "auto", I made the basic configuration

--- 2020.02.17.GitHubOp/auto.cs
+++ 2020.02.17.GitHubOp/auto.cs
@@ -8,5 +8,33 @@ namespace _2020._02._17.GitHubOp
 {
     class auto
     {
+        private string _marka;
+        private string _modell;
+        private int _evjarat;
+        private int _kilometerek;
+        private string _uzemanyagTipus;
+        private int _autoAr;
+
+        public auto(string marka, string modell, int evjarat, int kilometerek, string uzemanyagTipus, int autoAr)
+        {
+            Marka = marka;
+            Modell = modell;
+            Evjarat = evjarat;
+            Kilometerek = kilometerek;
+            UzemanyagTipus = uzemanyagTipus;
+            AutoAr = autoAr;
+        }
+
+        public string Marka { get => _marka; set => _marka = value; }
+        public string Modell { get => _modell; set => _modell = value; }
+        public int Evjarat { get => _evjarat; set => _evjarat = value; }
+        public int Kilometerek { get => _kilometerek; set => _kilometerek = value; }
+        public string UzemanyagTipus { get => _uzemanyagTipus; set => _uzemanyagTipus = value; }
+        public int AutoAr { get => _autoAr; set => _autoAr = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
---
+++ 2020.02.17.GitHubOp/autok.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2020._02._17.GitHubOp
+{
+    class autok
+    {
+        private List<auto> AutoLista = new List<auto>();
+        public autok(string fName)
+        {
+            using (StreamReader sr = new StreamReader(fName))
+            {
+                sr.ReadLine();
+            }
+
+        }
+    }
+}
